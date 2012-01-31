447b9f55d1cce668bc2f076909f4af92d4bdbcc8
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-01-31 22:14:05 +0000

    allow non-frameworks date finder to work

--- branches/servoySix/_dsa_sutra_DATE_date_picker/forms/DATE_P__search.js
+++ branches/servoySix/_dsa_sutra_DATE_date_picker/forms/DATE_P__search.js
@@ -415,6 +415,7 @@ switch (globals.DATE_date_range_type) {
 /**
  *
  * @properties={typeid:24,uuid:"6a5aff07-19a9-439a-99bf-3204526ced04"}
+ * @AllowToRunInFind
  */
 function ACTION_search()
 {
@@ -468,17 +469,20 @@ else {
 	//copy current foundset to restore if no records are found
 	var currentRecs = forms[globals.DATE_date_range_search_form].foundset.duplicateFoundSet()
 	
-	var searchDate = new Date()
-	searchDate = globals.DATE_date_range_entry
+	var searchDate = globals.DATE_date_range_entry + '|' + solutionPrefs.fastFind.dateFormat
+	
+	if (globals.DATE_date_range_type == 'Day') {
+		searchDate = '#' + searchDate
+	}
 	
 	//search calling form
 	forms[globals.DATE_date_range_search_form].controller.find()
 	
 	forms[globals.DATE_date_range_search_form][globals.DATE_date_range_field_name] = searchDate
 	
-	forms[globals.DATE_date_range_search_form].elements.fld_date_to_search.requestFocus()
+//	forms[globals.DATE_date_range_search_form].elements.fld_date_to_search.requestFocus()
 	
-	forms[globals.DATE_date_range_search_form].elements.fld_date_to_search.replaceSelectedText("")
+//	forms[globals.DATE_date_range_search_form].elements.fld_date_to_search.replaceSelectedText("")
 	
 	var count = forms[globals.DATE_date_range_search_form].controller.search()
 	
