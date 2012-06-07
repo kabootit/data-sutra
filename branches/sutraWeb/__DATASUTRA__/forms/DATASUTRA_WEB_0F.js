17c002fa929d3a756fc755379466ef931ef8223b
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-06-07 12:09:16 +0000

    callback to js as soon as webclient is ready

--- branches/sutraWeb/__DATASUTRA__/forms/DATASUTRA_WEB_0F.js
+++ branches/sutraWeb/__DATASUTRA__/forms/DATASUTRA_WEB_0F.js
@@ -53,6 +53,9 @@ function FORM_on_show(firstShow, event) {
 		elements.tab_wrapper.bgcolor = '#d1d7e2'
 		elements.tab_wrapper.resizeWeight = 1
 		
+		//update loading indicator
+		//plugins.WebClientUtils.executeClientSideJS('setIndicator();')
+		
 		//go to login form
 		forms.AC_R__login_WEB.controller.show()
 	}
