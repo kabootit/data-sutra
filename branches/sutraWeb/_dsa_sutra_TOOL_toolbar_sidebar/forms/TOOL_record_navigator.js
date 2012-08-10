14405a8e1995f84c643e5f3ba657dbc5b750ef4e
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-08-10 13:57:51 +0000

    get record navigator to show up correct first time visited

--- branches/sutraWeb/_dsa_sutra_TOOL_toolbar_sidebar/forms/TOOL_record_navigator.js
+++ branches/sutraWeb/_dsa_sutra_TOOL_toolbar_sidebar/forms/TOOL_record_navigator.js
@@ -2,36 +2,22 @@
  *
  * @properties={typeid:24,uuid:"99491a64-2a9a-466b-9b0d-aca6ecc22abd"}
  */
-function FORM_on_show()
-{
-
-/*
- *	TITLE    :	FORM_on_show
- *			  	
- *	MODULE   :	rsrc_TOOL_toolbar
- *			  	
- *	ABOUT    :	
- *			  	
- *	INPUT    :	
- *			  	
- *	OUTPUT   :	
- *			  	
- *	REQUIRES :	
- *			  	
- *	USAGE    :	FORM_on_show()
- *			  	
- *	MODIFIED :	July 24, 2008 -- Troy Elliott, Data Mosaic
- *			  	
- */
-
-if (application.__parent__.solutionPrefs) {
-	var formNameBase = solutionPrefs.config.formNameBase
-	
-	//if not 'previewing' the status area
-	if (solutionPrefs.config.currentFormName != 'MGR_0F_toolbar') {	
-		//update record navigator
-		globals.TRIGGER_toolbar_record_navigator_set()
+function FORM_on_show(firstShow,event) {
+	if (application.__parent__.solutionPrefs) {
+		//when first shown on web, make sure to double fire it after elements available in the DOM
+			//TODO: should also refire every time the window size was changed since last shown
+			//(servoy server doesn't know what size the elements currently are)
+		if (solutionPrefs.config.webClient && firstShow) {
+			//don't show indicator when this runs (we hope people don't notice it happens after the fact)
+			var callback = plugins.WebClientUtils.generateCallbackScript(globals.TRIGGER_toolbar_record_navigator_set,null,false)
+			var jsCallback = 'function recNavFirstShow(){' + callback + '}';
+			plugins.WebClientUtils.executeClientSideJS('recordNavigatorFirstShow(' + jsCallback + ');')
+		}
+		
+		//when 'previewing' this toolbar, disabled (on the form)
+		if (solutionPrefs.config.currentFormName != 'MGR_0F_toolbar') {	
+			//update record navigator
+			globals.TRIGGER_toolbar_record_navigator_set()
+		}
 	}
-}
-
 }
