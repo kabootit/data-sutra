0077935b1cac558dcf59e1c12b935bab48a38588
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-11-08 16:39:30 +0000

    allow right-click on sidebar header buttons

--- branches/sutraWeb/__DATASUTRA__/forms/DATASUTRA__sidebar__header.js
+++ branches/sutraWeb/__DATASUTRA__/forms/DATASUTRA__sidebar__header.js
@@ -2,62 +2,55 @@
  *
  * @properties={typeid:24,uuid:"6F34FC2B-7EED-4619-A8E9-D07E78B920AB"}
  */
-function ACTION_collapse()
-{
-
-if (application.__parent__.solutionPrefs) {
-	var flexMethod = (solutionPrefs.config.webClient) ? forms.DATASUTRA_WEB_0F__header.ACTION_space_flexible : globals.DS_space_flexible
-	var sideBarMethod = (solutionPrefs.config.webClient) ? globals.DS_sidebar_toggle : globals.DS_sidebar_toggle
-	
-	var splitToolFind = forms.DATASUTRA_WEB_0F__header.elements.split_tool_find
-	var divLocation = splitToolFind.dividerLocation
-	
-	//in flexible spaces
-	var flexOn = solutionPrefs.config.flexibleSpace
-	
-	if (flexOn) {
-		flexMethod(null,true)
+function ACTION_collapse() {
+	if (application.__parent__.solutionPrefs) {
+		var flexMethod = (solutionPrefs.config.webClient) ? forms.DATASUTRA_WEB_0F__header.ACTION_space_flexible : globals.DS_space_flexible
+		var sideBarMethod = (solutionPrefs.config.webClient) ? globals.DS_sidebar_toggle : globals.DS_sidebar_toggle
+		
+		var splitToolFind = forms.DATASUTRA_WEB_0F__header.elements.split_tool_find
+		var divLocation = splitToolFind.dividerLocation
+		
+		//in flexible spaces
+		var flexOn = solutionPrefs.config.flexibleSpace
+		
+		if (flexOn) {
+			flexMethod(null,true)
+		}
+		
+		sideBarMethod(false)
+		
+		if (flexOn) {
+			flexMethod(null,true)
+		}
+		
+		if (solutionPrefs.config.webClient) {
+			splitToolFind.dividerLocation = divLocation + solutionPrefs.screenAttrib.sidebar.currentSize
+		}
 	}
-	
-	sideBarMethod(false)
-	
-	if (flexOn) {
-		flexMethod(null,true)
-	}
-	
-	if (solutionPrefs.config.webClient) {
-		splitToolFind.dividerLocation = divLocation + solutionPrefs.screenAttrib.sidebar.currentSize
-	}
-}
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"E1D1E38A-CF0B-4CCC-92D7-CB20DA3E8540"}
  */
-function ACTION_popin()
-{
-
-
-globals.DS_toolbar_popout(false)
+function ACTION_popin() {
+	globals.DS_toolbar_popout(false)
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"A01DE310-60E2-4C6B-9467-14FF5772381A"}
  */
-function ACTION_popout()
-{
-
-
-globals.DS_toolbar_popout(true)
+function ACTION_popout() {
+	globals.DS_toolbar_popout(true)
 }
 
 /**
+ * @param {JSEvent} event
+ * 
  * @properties={typeid:24,uuid:"6FE3D5FD-3BBC-4ED2-A894-8CD08B488FA2"}
  */
-function ACTIONS_popdown(event)
-{
+function ACTIONS_popdown(event) {
 	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
 
 	if (forms[sideForm] && forms[sideForm].ACTIONS_list) {
@@ -66,120 +59,111 @@ function ACTIONS_popdown(event)
 }
 
 /**
+ * @param {JSEvent} event
+ * 
+ * @properties={typeid:24,uuid:"14B987BB-945A-45B2-ADE4-A074604F522F"}
+ */
+function ACTIONS_popdown_right(event) {
+	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
+
+	if (forms[sideForm] && forms[sideForm].ACTIONS_list_right) {
+		forms[sideForm].ACTIONS_list_right(event)
+	}
+}
+
+/**
+ * Toggle which buttons are showing
  *
  * @properties={typeid:24,uuid:"094601EE-82AD-4637-9344-09B193F79D45"}
  */
-function BUTTONS_toggle()
-{
-
-/*
- *	TITLE    :	BUTTONS_toggle
- *			  	
- *	MODULE   :	rsrc_TOOL_toolbar
- *			  	
- *	ABOUT    :	toggle which buttons are showing
- *			  	
- *	INPUT    :	
- *			  	
- *	OUTPUT   :	
- *			  	
- *	REQUIRES :	
- *			  	
- *	USAGE    :	BUTTONS_toggle()
- *			  	
- *	MODIFIED :	July 24, 2008 -- Troy Elliott, Data Mosaic
- *			  	
- */
-
-//what form are we operating on?
-if (forms.DATASUTRA__sidebar.elements.tab_content.tabIndex) {
-	var tabForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
-
-	var sideBar = solutionPrefs.panel.sidebar[forms.DATASUTRA__sidebar.elements.tab_content.tabIndex - 2]
-}
-
-//color and gradient options
-if (sideBar) {
-	//do the color
-	if (sideBar.gradientColor) {
-		forms.DATASUTRA__sidebar.elements.lbl_color.bgcolor = sideBar.gradientColor
-		forms.DATASUTRA__sidebar.elements.lbl_color.visible = true
-	}
-	else {
-		forms.DATASUTRA__sidebar.elements.lbl_color.visible = false
+function BUTTONS_toggle() {
+	//what form are we operating on?
+	if (forms.DATASUTRA__sidebar.elements.tab_content.tabIndex) {
+		var tabForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
+	
+		var sideBar = solutionPrefs.panel.sidebar[forms.DATASUTRA__sidebar.elements.tab_content.tabIndex - 2]
 	}
 	
-	//do the gradient
-	forms.DATASUTRA__sidebar.elements.gfx_gradient.visible = sideBar.gradient
+	//color and gradient options
+	if (sideBar) {
+		//do the color
+		if (sideBar.gradientColor) {
+			forms.DATASUTRA__sidebar.elements.lbl_color.bgcolor = sideBar.gradientColor
+			forms.DATASUTRA__sidebar.elements.lbl_color.visible = true
+		}
+		else {
+			forms.DATASUTRA__sidebar.elements.lbl_color.visible = false
+		}
+		
+		//do the gradient
+		forms.DATASUTRA__sidebar.elements.gfx_gradient.visible = sideBar.gradient
+		
+		//set title if more than one sidebar
+		if (solutionPrefs.panel.sidebar.length > 1) {
+	//		forms.DATASUTRA__sidebar__header.elements.lbl_heading.text = (sideBar.tabName) ? sideBar.tabName.toUpperCase() : ''
+			forms.DATASUTRA__sidebar__header.elements.lbl_heading.toolTipText = (sideBar.tabName) ? sideBar.tabName : ''
+		}
+		//otherwise blank out title text
+		else {
+			forms.DATASUTRA__sidebar__header.elements.lbl_heading.text = ''
+			forms.DATASUTRA__sidebar__header.elements.lbl_heading.toolTipText = ''
+		}
+	}
 	
-	//set title if more than one sidebar
-	if (solutionPrefs.panel.sidebar.length > 1) {
-//		forms.DATASUTRA__sidebar__header.elements.lbl_heading.text = (sideBar.tabName) ? sideBar.tabName.toUpperCase() : ''
-		forms.DATASUTRA__sidebar__header.elements.lbl_heading.toolTipText = (sideBar.tabName) ? sideBar.tabName : ''
+	//toggle add record button
+	if (tabForm && forms[tabForm] && forms[tabForm].REC_new) {
+		elements.btn_add.visible = true
 	}
-	//otherwise blank out title text
 	else {
-		forms.DATASUTRA__sidebar__header.elements.lbl_heading.text = ''
-		forms.DATASUTRA__sidebar__header.elements.lbl_heading.toolTipText = ''
+		elements.btn_add.visible = false
 	}
-}
-
-//toggle add record button
-if (tabForm && forms[tabForm] && forms[tabForm].REC_new) {
-	elements.btn_add.visible = true
-}
-else {
-	elements.btn_add.visible = false
-}
-
-//toggle delete record button
-if (tabForm && forms[tabForm] && forms[tabForm].REC_delete) {
-	elements.btn_delete.visible = true
-}
-else {
-	elements.btn_delete.visible = false
-}
-
-//toggle actions button
-if (tabForm && forms[tabForm] && forms[tabForm].ACTIONS_list) {
-	elements.btn_actions.visible = true
-}
-else {
-	elements.btn_actions.visible = false
-}
-
-/*
-//toggle xxx button
-if (tabForm && forms[tabForm] && forms[tabForm].) {
-	elements.btn_add.visible = true
-}
-else {
-	elements.btn_add.visible = false
-}
-*/
-
-
+	
+	//toggle delete record button
+	if (tabForm && forms[tabForm] && forms[tabForm].REC_delete) {
+		elements.btn_delete.visible = true
+	}
+	else {
+		elements.btn_delete.visible = false
+	}
+	
+	//toggle actions button
+	if (tabForm && forms[tabForm] && forms[tabForm].ACTIONS_list) {
+		elements.btn_actions.visible = true
+	}
+	else {
+		elements.btn_actions.visible = false
+	}
+	
+	/*
+	//toggle xxx button
+	if (tabForm && forms[tabForm] && forms[tabForm].) {
+		elements.btn_add.visible = true
+	}
+	else {
+		elements.btn_add.visible = false
+	}
+	*/
 }
 
 /**
  *
  * @properties={typeid:24,uuid:"09060AAE-8FDF-4C19-ABF7-C0F76EE807EC"}
  */
-function FORM_on_load()
-{
-//elements.btn_popin.visible = false
-elements.gfx_flexible.visible = false
-
-if (solutionPrefs.config.webClient) {
-	elements.btn_collapse.toolTipText = 'Collapse sidebar'
-}
+function FORM_on_load() {
+	//elements.btn_popin.visible = false
+	elements.gfx_flexible.visible = false
+	
+	if (solutionPrefs.config.webClient) {
+		elements.btn_collapse.toolTipText = 'Collapse sidebar'
+	}
 }
 
 /**
+ * @param {JSEvent} event
+ *
  * @properties={typeid:24,uuid:"D6B4D12F-261F-4905-9AF8-B562CD862B9D"}
  */
-function REC_delete(event)
-{
+function REC_delete(event) {
 	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
 
 	if (forms[sideForm] && forms[sideForm].REC_delete) {
@@ -188,14 +172,43 @@ function REC_delete(event)
 }
 
 /**
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"A73C6E65-09EF-42F0-9768-B535FC3B64A7"}
+ */
+function REC_delete_right(event) {
+	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
+
+	if (forms[sideForm] && forms[sideForm].REC_delete_right) {
+		forms[sideForm].REC_delete_right(event)
+	}
+}
+
+/**
+ * @param {JSEvent} event
+ * 
  * @properties={typeid:24,uuid:"FAA9E8DA-AC22-4EBC-BD99-8307CE76A2C8"}
  */
-function REC_new(event)
-{
+function REC_new(event) {
 	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
 
 	if (forms[sideForm] && forms[sideForm].REC_new) {
 		forms[sideForm].REC_new(event)
+	}
+}
+
+/**
+ * Perform the element right-click action.
+ *
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"8FE2C257-FDFB-4BB8-BAED-58346BD0B240"}
+ */
+function REC_new_right(event) {
+	var sideForm = forms.DATASUTRA__sidebar.elements.tab_content.getTabFormNameAt(forms.DATASUTRA__sidebar.elements.tab_content.tabIndex)
+
+	if (forms[sideForm] && forms[sideForm].REC_new_right) {
+		forms[sideForm].REC_new_right(event)
 	}
 }
 
@@ -332,6 +345,5 @@ function TAB_popdown(input) {
 			//show sidebar
 			globals.DS_sidebar_toggle(true,null,true)
 		}
-	
 	}
 }
