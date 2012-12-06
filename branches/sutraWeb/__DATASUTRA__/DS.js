422b369b0b73083ed463f57dab8baf4c164e7f82
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-12-06 16:28:08 +0000

    helper method to reattach subheader graphic to css gradient throughout included forms

--- branches/sutraWeb/__DATASUTRA__/DS.js
+++ branches/sutraWeb/__DATASUTRA__/DS.js
@@ -16,3 +16,27 @@ function setFactor(device) {
 		deviceFactor = device
 	}
 }
+
+/**
+ * On show method for form that will hook up css class to subheader graphic
+ * @param {Boolean} firstShow
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"5F9F8E54-8AED-4C2B-8DDC-542AC91B0D40"}
+ */
+function subheaderWeb(firstShow, event) {
+	if (firstShow && event instanceof JSEvent) {
+		var formName = event.getFormName()
+		var allElems = forms[formName].elements.allnames
+		var smForm = solutionModel.getForm(formName)
+		
+		for (var i = 0; i < allElems.length; i++) {
+			var elem = allElems[i]
+			var smElem = smForm.getComponent(elem)
+			
+			if (smElem.styleClass == 'gfx_subheader') {
+				plugins.WebClientUtils.setExtraCssClass(forms[formName].elements[elem], 'gfxSubHeader')
+			}
+		}
+	}
+}
