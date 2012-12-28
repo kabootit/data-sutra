463ac99cb000bef812b16deec78cd1e939fd4c6f
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2012-12-28 17:48:30 +0000

    remove atob custom js function

--- branches/sutraWeb/__DATASUTRA__/globals.js
+++ branches/sutraWeb/__DATASUTRA__/globals.js
@@ -5288,55 +5288,6 @@ function DS_router(p1,params,itemID,launch,logout,pathName) {
 		}
 	}
 	
-	//helper function to unbase64 (for referrer)
-	function atob(input) {
-		var output = "";
-		var chr1, chr2, chr3 = "";
-		var enc1, enc2, enc3, enc4 = "";
-		var i = 0;
-		
-		var keyStr = "ABCDEFGHIJKLMNOP" +
-					"QRSTUVWXYZabcdef" +
-					"ghijklmnopqrstuv" +
-					"wxyz0123456789+/" +
-					"=";
-
-		// remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-		var base64test = /[^A-Za-z0-9\+\/\=]/g;
-		if (base64test.exec(input)) {
-			application.output("There were invalid base64 characters in the input text.\n" +
-				 "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-				 "Expect errors in decoding.");
-		}
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-		do {
-			enc1 = keyStr.indexOf(input.charAt(i++));
-			enc2 = keyStr.indexOf(input.charAt(i++));
-			enc3 = keyStr.indexOf(input.charAt(i++));
-			enc4 = keyStr.indexOf(input.charAt(i++));
-
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-
-			output = output + String.fromCharCode(chr1);
-
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
-
-			chr1 = chr2 = chr3 = "";
-			enc1 = enc2 = enc3 = enc4 = "";
-
-		} while (i < input.length);
-
-		return unescape(output);
-	}
-	
 	//helper function to return url of navitem to be navigated to
 	function getURL(alt) {
 		var urlString = prefix
@@ -5419,8 +5370,6 @@ function DS_router(p1,params,itemID,launch,logout,pathName) {
 			//TODO: should probably put this into the NCRYPT library
 			var fromBase64 = new Packages.sun.misc.BASE64Decoder
 			url.referrer = scopes.NCRYPT.util.UTF8.bytesToString(fromBase64.decodeBuffer(params[item]))
-			
-//			url.referrer = decodeURIComponent(escape(atob(params[item])))
 			
 			//store down referrer for this session
 			if (!globals.DATASUTRA_router_referrer && !globals.DATASUTRA_router.length) {
