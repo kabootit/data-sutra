ea5c0001c1ba05d2ada4aaa6fca89e709a8b5942
troy <troy@da3f7bfc-eafd-42a0-b953-57e6d4dd33b8>
2013-01-04 19:43:11 +0000

    lighten up the light background color

--- branches/sutraWeb/_ds_CODE_resources/DS.js
+++ branches/sutraWeb/_ds_CODE_resources/DS.js
@@ -82,7 +82,7 @@ function webFactorSet(device) {
  *
  * @properties={typeid:24,uuid:"5F9F8E54-8AED-4C2B-8DDC-542AC91B0D40"}
  */
-function webSubheader(firstShow, event) {
+function webStyleCSS(firstShow, event) {
 	if (firstShow && event instanceof JSEvent) {
 		var formName = event.getFormName()
 		var allElems = forms[formName].elements.allnames
@@ -92,8 +92,14 @@ function webSubheader(firstShow, event) {
 			var elem = allElems[i]
 			var smElem = smForm.getComponent(elem)
 			
+			//subheader images
 			if (smElem && smElem.styleClass == 'gfx_subheader') {
 				plugins.WebClientUtils.setExtraCssClass(forms[formName].elements[elem], 'gfxSubHeader')
+			}
+			
+			//light boxes to delineate input field areas
+			if (smElem && smElem.styleClass == 'color_light') {
+				plugins.WebClientUtils.setExtraCssClass(forms[formName].elements[elem], 'gfxColorLight')
 			}
 		}
 	}
